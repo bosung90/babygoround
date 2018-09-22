c4c029fbecd8a5028b4206d826f809f4ede4121f
Eric Kim <bosung90@gmail.com>
2018-09-22 14:25:23 -0700

    add header

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Router, Route, Switch } from 'react-router-dom';
-import history from './history';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { Router, Route, Switch } from 'react-router-dom'
+import history from './history'
+import { Header } from 'components'
 
 ReactDOM.render(
-    <Router history={ history }>
-        <div>
-            /* This will be some default component that will show for all pages
-             * e.g Header
-             */
-            <Switch>
-                <Route exact path="/something" component={ App }/>
-                {/* <Route path="/something" component={add your component here}/> */}
-            </Switch>
-        </div>
-    </Router>
-    , document.getElementById('root'));
-    registerServiceWorker();
+  <Router history={history}>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/something" component={App} />
+        {/* <Route path="/something" component={add your component here}/> */}
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById('root')
+)
+registerServiceWorker()

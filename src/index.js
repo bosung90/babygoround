44b9d0a4ef2c7620074d077c3086e23cc6d5e451
Jasonhjkim <jasonhjkim1205@gmail.com>
2018-09-22 14:22:15 -0700

    Added react-router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Router history={ history }>
+        <div>
+            /* This will be some default component that will show for all pages
+             * e.g Header
+             */
+            <Switch>
+                <Route exact path="/something" component={ App }/>
+                {/* <Route path="/something" component={add your component here}/> */}
+            </Switch>
+        </div>
+    </Router>
+    , document.getElementById('root'));
+    registerServiceWorker();

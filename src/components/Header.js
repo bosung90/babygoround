3fcc36b00b2d0481913b75a19fabf60183b19983
Eric Kim <bosung90@gmail.com>
2018-09-22 14:38:56 -0700

    added emotion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { css } from 'react-emotion'
 
 export default class Header extends React.Component {
   render() {
-    return <div>Header</div>
+    return <div className={styles.header}>Header</div>
   }
+}
+
+const styles = {
+  header: css({}),
 }

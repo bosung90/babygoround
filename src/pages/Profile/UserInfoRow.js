5ae223dace52bc3999cddd8caabb4603d6749e57
Pooya Daravi <daravi@alumni.ubc.ca>
2018-09-23 12:42:40 -0700

    Fix profile page

--- src/pages/Profile/UserInfoRow.js
+++ src/pages/Profile/UserInfoRow.js
@@ -7,9 +7,9 @@ export default class UserInfoRow extends React.Component {
     const { label, value, ...props } = this.props
     return (
       <View row h={25} alignCenter style={{ marginBottom: 10 }}>
-        <Label w={120} mr={35}>
-          {label}
-        </Label>
+        <View w={130} mr={35}>
+          <Label label={label} />
+        </View>
         <View>{value}</View>
       </View>
     )

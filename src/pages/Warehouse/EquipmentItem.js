079a398cd9b71e433214cf9dad52c693259d70e9
Eric Kim <bosung90@gmail.com>
2018-09-23 13:07:50 -0700

    click to add requested to checkedOutItems

--- src/pages/Warehouse/EquipmentItem.js
+++ src/pages/Warehouse/EquipmentItem.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import { View, Button } from 'components'
 import { colors } from 'common'
-import { dispatch } from 'store'
+import { dispatch, getState } from 'store'
+import { firestore } from 'firebase/config'
 
 export default class EquipmentItem extends React.Component {
   render() {
     const { mr, equipment, name } = this.props
     return (
-      <View mr={mr} column alignCenter>
+      <View
+        onClick={() => {
+          const checkedOutEquipments = getState().User.checkedOutEquipments
+          firestore
+            .collection('Users')
+            .doc(dispatch.User.getUserId())
+            .update({
+              checkedOutEquipments: {
+                ...checkedOutEquipments,
+                [equipment.id]: true,
+              },
+            })
+        }}
+        mr={mr}
+        column
+        alignCenter
+      >
         {equipment.imageURL ? (
           <img
             style={{

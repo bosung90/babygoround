a09985491e2f643fe96043853e4c50095b361852
Eric Kim <bosung90@gmail.com>
2018-09-22 23:59:39 -0700

    don't render Header when in /auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,31 @@ import { View, Button } from 'components'
 import { colors } from 'common'
 import { headerLogo } from 'images'
 import { withRouter } from 'react-router-dom'
+import Select from 'cf-select'
 
 export default withRouter(
   class Header extends React.Component {
     render() {
       return (
-        <View row alignCenter h={75} className={styles.header} justifyBetween>
-          <Button className={styles.logoButton}>
-            <img alt="header-logo" src={headerLogo} />
-          </Button>
-          <View row w={220} mr={20} justifyBetween>
-            <Button secondary className={{ width: 100, height: 40 }}>
-              Profile
+        <Select selector={this.props.location.pathname !== '/auth'}>
+          <View row alignCenter h={75} className={styles.header} justifyBetween>
+            <Button className={styles.logoButton}>
+              <img alt="header-logo" src={headerLogo} />
             </Button>
-            <Button
-              onClick={() => this.props.history.push('./auth')}
-              secondary
-              className={{ width: 100, height: 40 }}
-            >
-              Logout
-            </Button>
+            <View row w={220} mr={20} justifyBetween>
+              <Button secondary className={{ width: 100, height: 40 }}>
+                Profile
+              </Button>
+              <Button
+                onClick={() => this.props.history.push('./auth')}
+                secondary
+                className={{ width: 100, height: 40 }}
+              >
+                Logout
+              </Button>
+            </View>
           </View>
-        </View>
+        </Select>
       )
     }
   }

af694e6b3ede9c2fd1f7f1ce3430aba9447f9cb0
Eric Kim <bosung90@gmail.com>
2018-09-23 00:01:56 -0700

    redirect / to /auth

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { Router, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import history from './history'
 import { Header, View } from 'components'
 import { Provider } from 'react-redux'
@@ -25,9 +25,9 @@ ReactDOM.render(
         <div>
           <Header />
           <Switch>
-            <Route exact path="/" component={App} />
-            <Route exact path="/something" component={App} />
+            <Route exact path="/" component={() => <Redirect to="/auth" />} />
             <Route exact path="/auth" component={pages.Auth} />
+            <Route exact path="/something" component={App} />
             <Route exact path="/form" component={pages.Form} />
             <Route exact path="/calendar" component={pages.Calendar} />
             <Route exact path="/register" component={RegistrationFormOne} />

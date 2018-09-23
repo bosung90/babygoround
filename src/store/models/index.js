a0397ca7403a13503f5e436aa78b2f5d1dcf91e9
Eric Kim <bosung90@gmail.com>
2018-09-23 01:00:56 -0700

    remove unused commented out code

--- src/store/models/index.js
+++ src/store/models/index.js
@@ -2,25 +2,3 @@ import User from './User'
 import Equipments from './Equipments'
 
 export { User, Equipments }
-
-// export default {
-//   count: {
-//     state: 0,
-//     reducers: {
-//       increment(state, payload) {
-//         return state + payload
-//       },
-//     },
-//     actions: ({ dispatch, getState }) => ({
-//       getCount() {
-//         return getState().count
-//       },
-//       async incrementAsync() {
-//         await new Promise(resolve => setTimeout(resolve, 1000))
-//         dispatch.count.increment(1)
-//       },
-//     }),
-//   },
-//   User,
-//   Equipments,
-// }
